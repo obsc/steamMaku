6b48d0480d2c9d867b74908457363f4072e6dcd3
Obscuron <Abscuron@gmail.com>
2013-11-23 20:26:14 -0500

    more quick fixes

--- src/game/player.ml
+++ src/game/player.ml
@@ -11,8 +11,16 @@ let get_start_pos (c : color) : position =
     | Red  -> 0.125
     | Blue -> 0.875 in
   let x : float = m *. (float_of_int cBOARD_WIDTH) in
-  let y : float = float_of_int cBOARD_HEIGHT in
+  let y : float = 0.5 *. (float_of_int cBOARD_HEIGHT) in
   (x, y)
+
+let initGui (id : id) (c : color) (pos : position) : unit =
+  add_update (AddPlayer (id, c, pos));
+  add_update (SetLives (c, cINITIAL_LIVES));
+  add_update (SetBombs (c, cINITIAL_BOMBS));
+  add_update (SetScore (c, 0));
+  add_update (SetPower (c, 0));
+  add_update (SetCharge (c, 0))
 
 let create (c : color) : t =
   let id : id = next_available_id () in
@@ -22,7 +30,7 @@ let create (c : color) : t =
                           p_focused = false;
                           p_radius = cHITBOX_RADIUS;
                           p_color = c } in
-  add_update (AddPlayer (id, c, pos));
+  initGui id c pos;
   (ref (cINITIAL_LIVES, cINITIAL_BOMBS, 0, 0, 0, p), ref [])
 
 let update (x : t) : unit = ()
