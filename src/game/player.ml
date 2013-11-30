783dde8ed87a9f5d8a36f04ed6d0b03909a45db5
Obscuron <Abscuron@gmail.com>
2013-11-30 17:42:59 -0500

    refactored player to use mutable records

--- src/game/player.ml
+++ src/game/player.ml
@@ -4,15 +4,25 @@ open Gameutil
 open Util
 open Netgraphics
 
-(* lives, bombs, score, power, charge, player, move list, focus *)
-type team = team_data ref
-type moves = (direction * direction) list ref
-type focus = bool ref
 type invulnerable = | Normal
                     | Mercy of int
                     | Bombing of int
 
-type t = team * moves * focus * invulnerable ref
+type t = {
+  mutable lives : int;
+  mutable bombs : int;
+  mutable score : int;
+  mutable power : int;
+  mutable charge : int;
+  mutable moves : (direction * direction) list;
+  mutable id : int;
+  mutable pos : position;
+  mutable focused : bool;
+  mutable radius : int;
+  mutable color : color;
+  mutable status : invulnerable
+}
+
 type cons = color
 
 (* Gets the speed based on its focus state *)
@@ -39,125 +49,111 @@ let initGui (id : id) (c : color) (pos : position) : unit =
 
 (* Called upon receiving a move action *)
 let setMoves (x : t) (lst : (direction * direction) list) : unit =
-  match x with (t, moves, f, m) -> moves := lst
+  x.moves <- lst
 
 (* Called upon receiving a focus action *)
 let setFocus (x : t) (b : bool) : unit =
-  match x with (t, moves, f, m) -> f := b
+  x.focused <- b
 
 (* Instantiates a player *)
 let create (c : color) : t =
   let id : id = next_available_id () in
   let pos : position = getStartPos c in
-  let p : player_char = { p_id = id;
-                          p_pos = pos;
-                          p_focused = false;
-                          p_radius = cHITBOX_RADIUS;
-                          p_color = c } in
   initGui id c pos;
-  let team : team_data = (cINITIAL_LIVES, cINITIAL_BOMBS, 0, 0, 0, p) in
-  (ref team, ref [], ref false, ref Normal)
+  { lives = cINITIAL_LIVES;
+    bombs = cINITIAL_BOMBS;
+    score = 0;
+    power = 0;
+    charge = 0;
+    moves = [];
+    id = id;
+    pos = pos;
+    focused = false;
+    radius = cHITBOX_RADIUS;
+    color = c;
+    status = Normal }
 
 (* Moves a position, bounding it to inside the field *)
-let moveOff (pos : position) (off : vector) : position =
-  let new_pos  = add_v pos off in
+let move (pos : position) (offset : vector) : position =
+  let new_pos  = add_v pos offset in
   if in_bounds new_pos then new_pos else pos
 
 (* A single update step: updates focus and position *)
 let update (x : t) : unit =
-  match x with (t, moves, f, m) ->
-  match !t with (l, b, s, p, c, player) ->
   (* Calculates the amount the player needs to move by *)
-  let off : float * float = match !moves with
+  let off : float * float = match x.moves with
     | []   -> (0., 0.)
-    | h::t -> vector_of_dirs h (getSpeed !f) in
-  let pos : position = moveOff player.p_pos off in
-  let new_player : player_char = 
-    { player with p_pos = pos; p_focused = !f } in
-  add_update (MovePlayer (player.p_id, pos));
-  t := (l, b, s, p, c, new_player);
+    | h::t -> vector_of_dirs h (getSpeed x.focused) in
+  x.pos <- move x.pos off;
+  add_update (MovePlayer (x.id, x.pos));
   (* Updates invulnerability timers *)
-  match !m with
+  match x.status with
     | Normal    -> ()
-    | Mercy n   -> if n = 0 then m := Normal else m := Mercy (n - 1)
-    | Bombing n -> if n = 0 then m := Normal else m := Bombing (n - 1)
+    | Mercy n   -> if n = 0 then x.status <- Normal
+                   else x.status <- Mercy (n - 1)
+    | Bombing n -> if n = 0 then x.status <- Normal
+                   else x.status <- Bombing (n - 1)
 
 (* Updates charge of the player *)
 let updateCharge (x : t) : unit =
-  match x with (t, moves, f, m) ->
-  match !t with (l, b, s, p, c, player) ->
-  let charge : int = min (c + cCHARGE_RATE + p) cCHARGE_MAX in
-  add_update (SetCharge (player.p_color, charge));
-  t := (l, b, s, p, charge, player)
+  x.charge <- min (x.charge + cCHARGE_RATE + x.power) cCHARGE_MAX;
+  add_update (SetCharge (x.color, x.charge))
 
-(* Uses up cost amount of charge to shoot a bullet *)
+(* Uses up cost amount of charge to shoot a bullet
+ * Returns true if successful *)
 let reduceCharge (x : t) (cost : int) : bool =
-  match x with (t, moves, f, m) ->
-  match !t with (l, b, s, p, c, player) ->
-  if c >= cost then begin
-    t := (l, b, s, p, c - cost, player); true
+  if x.charge >= cost then begin
+    x.charge <- x.charge - cost; true
   end else false
 
-(* Player is hit by an enemy bullet*)
+(* Player is hit by an enemy bullet 
+ * Returns true to destroy colliding bullet *)
 let hit (event : unit -> unit) (x : t) : bool =
-  match x with (t, moves, f, m) ->
-  match !m with
+  match x.status with
     | Normal -> begin
-      match !t with (l, b, s, p, c, player) ->
-      add_update (SetLives (player.p_color, l - 1));
-      add_update (SetBombs (player.p_color, cINITIAL_BOMBS));
-      add_update (SetPower (player.p_color, p / 2));
-      t := (l - 1, cINITIAL_BOMBS, s, p / 2, c, player);
-      m := Mercy cINVINCIBLE_FRAMES;
+      x.lives <- x.lives - 1;
+      x.bombs <- cINITIAL_BOMBS;
+      x.power <- x.power / 2;
+      x.status <- Mercy cINVINCIBLE_FRAMES;
+      add_update (SetLives (x.color, x.lives));
+      add_update (SetBombs (x.color, x.bombs));
+      add_update (SetPower (x.color, x.power));
       event ();
       true
     end
     | _      -> true
 
-(* Player is grazing an enemy bullet *)
+(* Player is grazing an enemy bullet 
+ * Returns false to not destroy bullet *)
 let graze (x : t) : bool =
-  match x with (t, moves, f, m) ->
-  match !t with (l, b, s, p, c, player) ->
-  add_update (SetScore (player.p_color, s + cGRAZE_POINTS));
+  x.score <- x.score + cGRAZE_POINTS;
+  add_update (SetScore (x.color, x.score));
   add_update (Graze);
-  t := (l, b, s + cGRAZE_POINTS, p, c, player);
   false
 
 (* Has killed other player: increases score *)
 let killedOther (x : t) : unit =
-  match x with (t, moves, f, m) ->
-  match !t with (l, b, s, p, c, player) ->
-  add_update (SetScore (player.p_color, s + cKILL_POINTS));
-  t := (l, b, s + cKILL_POINTS, p, c, player)
+  x.score <- x.score + cKILL_POINTS;
+  add_update (SetScore (x.color, x.score))
 
 (* Getters for player state *)
-let getPlayer (x : t) : player_char =
-  match x with (t, moves, f, m) ->
-  match !t with (l, b, s, p, c, player) ->
-  player
+let getHitbox (x : t) : hitbox = (x.pos, float_of_int x.radius)
 
-let getHitbox (x : t) : hitbox =
-  let p = getPlayer x in (p.p_pos, float_of_int p.p_radius)
+let getGrazebox (x : t) : hitbox = (x.pos, float_of_int cGRAZE_RADIUS)
 
-let getGrazebox (x : t) : hitbox =
-  let p = getPlayer x in (p.p_pos, float_of_int cGRAZE_RADIUS)
+let getPos (x : t) : position = x.pos
 
-let getPos (x : t) : position =
-  let p = getPlayer x in p.p_pos
+let getColor (x : t) : color = x.color
 
-let getColor (x : t) : color =
-  let p = getPlayer x in p.p_color
+let getScore (x : t) : int = x.score
 
-let getScore (x : t) : int =
-  match x with (t, moves, f, m) ->
-  match !t with (l, b, s, p, c, player) ->
-  s
-
-let dead (x : t) : bool =
-  match x with (t, moves, f, m) ->
-  match !t with (l, b, s, p, c, player) ->
-  l <= 0
+let dead (x : t) : bool = x.lives <= 0
 
 (* Returns team data of the player *)
 let getData (x : t) : team_data =
-  match x with (t, moves, f, m) -> !t
+  let player : player_char = { p_id = x.id;
+                               p_pos = x.pos;
+                               p_focused = x.focused;
+                               p_radius = x.radius;
+                               p_color = x.color } in
+  (x.lives, x.bombs, x.score, x.power, x.charge, player)
